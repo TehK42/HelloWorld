f596fe4a37d2178a83eaa4d0f7c32911a4c810a3
unknown <tupazluisgabriel@gmail.com>
2021-03-23 11:43:23 +0800

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,15 @@
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        int myNumba = (10 + 5) + (2 * 10);
+        int mySecondNumba = 12;
+        int myThirdNumba = myNumba * 2;
+        int myTotal = myNumba + mySecondNumba + myThirdNumba;
+        int myLastOne = 1000 - myTotal;
+        System.out.println(myTotal);
+        System.out.println(myLastOne);
+
+    }
+}
